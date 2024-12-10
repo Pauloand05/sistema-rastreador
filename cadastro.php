e41b286b3a418977bd72656459d23a7caf766db2
Pauloand05 <pauloandre050619@gmail.com>
2024-12-09 22:51:45 -0300

    login e cadastro

---
+++ cadastro.php
@@ -0,0 +1,54 @@
+<?php
+
+    //colocar o código php do backend
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div id="login">
+
+        <div class="caixa">
+
+            <img src="" alt="">
+
+            <h1>CADASTRO</h1>
+
+            <div class="nome">
+                <input type="text" placeholder="Nome">
+            </div>
+
+            <div class="telefone">
+                <input type="text" placeholder="Telefone">
+            </div>
+            
+            <div class="email">
+                <input type="email" placeholder="E-mail">
+            </div>
+
+            <div class="senha">
+                <input type="password" placeholder="Senha">
+            </div>
+
+            <div class="senha">
+                <input type="password" placeholder="Confirme sua senha">
+            </div>
+
+            <div class="entrar">
+                <p>Já tem uma conta? <a href="login.php">Entre Aqui!</a></p>
+                <input type="submit" value="Entrar">
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
