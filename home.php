3165210e72f47c062bdcdd7444cc6a1f9c683572
Pauloand05 <pauloandre050619@gmail.com>
2024-12-13 19:59:50 -0300

    Style

--- home.php
+++ home.php
@@ -8,40 +8,48 @@
     <title>AF RASTREADORES</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary" >
-    <div class="container-fluid">
-        <img src="img/logo-af.png" alt="Logo-AF" class="logo-nav">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
-            <li class="nav-item me-3">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-            </li>
-            <li class="nav-item me-3">
-                <a class="nav-link" href="#">Planos</a>
-            </li>
-            <li class="nav-item dropdown me-3">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Categoria
-                </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Moto</a></li>
-                <li><a class="dropdown-item" href="#">Carro</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Caminhão</a></li>
-            </ul>
-            </li>
-            <li class="nav-item me-3">
-                <a class="nav-link" href="#">Dúvidas</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contato</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Sobre Nós</a>
-            </li>
-        </ul>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <!-- Logo -->
+            <img src="img/logo-af.png" alt="Logo-AF" class="logo-nav">
+            
+            <!-- Botão hamburger alinhado à direita -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <!-- Links da Navbar -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto"> <!-- Alinha os itens à direita -->
+                    <li class="nav-item me-3">
+                        <a class="nav-link active" aria-current="page" href="#">Início</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#">Planos</a>
+                    </li>
+                    <li class="nav-item dropdown me-3">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categoria
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Moto</a></li>
+                            <li><a class="dropdown-item" href="#">Carro</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Caminhão</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#">Dúvidas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre Nós</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
     </nav>
 
     <script src="js/bootstrap.bundle.min.js"></script>
